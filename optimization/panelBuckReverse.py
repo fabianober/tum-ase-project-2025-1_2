e7101d46898dd01cd979e7586934cfdc35e55446
YannisEndr <yannis@endress-hg.de>
2025-06-14 21:52:16 +0200

    Did already a lot :). Kind of reverse engineered the panels

---
+++ optimization/panelBuckReverse.py
@@ -0,0 +1,32 @@
+import math 
+RF_goal = 0.9
+
+
+# Function for calculating the 
+def reverseBiaxial(RF_goal, sigma_applied, k_bi, EModulus, nu, width):
+    sigma_crit = abs(RF_goal * sigma_applied)
+    thickness = width * math.sqrt(12* (sigma_crit/(k_bi*EModulus * math.pi**2)) * (1-nu**2))
+    return thickness
+
+def reverseShear(RF_goal, tau_applied, k_shear, EModulus, nu, width):
+    tau_crit = abs(RF_goal*tau_applied)
+    thickness = width * math.sqrt(12* (tau_crit/(k_shear*EModulus * math.pi**2)) * (1-nu**2))
+    return thickness
+
+def panelBuckReverse(row, EModulus, nu):
+    # For biaxial we first need to check the principal direction 
+    if row['sigmaYY'] < row['sigmaXX']: 
+        #Swap stresses 
+        sigma_appliedBi = row['sigmaYY']
+        #swap dimensions
+        widthBi = row['length']
+    else:
+        sigma_appliedBi = row['sigmaXX']
+        widthBi = row['width']
+    
+
+    # Compute the reverse thicnesses 
+    thicknessBi = reverseBiaxial(RF_goal=RF_goal, sigma_applied=sigma_appliedBi, k_bi=row['k_biaxial'], EModulus=EModulus, nu=nu, width=widthBi)
+    thicknessShear = reverseShear(RF_goal=RF_goal, tau_applied=row['sigmaXY'], k_shear=row['k_shear'], EModulus=EModulus, nu=nu, width=row['width'])
+    thickness = max(thicknessBi, thicknessShear)
+    return thickness
