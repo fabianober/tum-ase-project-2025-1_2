f4cc25de416fd6536e5ad87b6327000f6ebf29da
YannisEndr <yannis@endress-hg.de>
2025-06-15 20:43:17 +0200

    Defined the outline for changing the element offset

--- hmscript/change_properties.py
+++ hmscript/change_properties.py
@@ -3,9 +3,32 @@ import hm.entities as ent
 
 model = hm.Model()
 
-parameters = hm.Collection(model,ent.Parameter)
+
+#Offsets panel elements to the correct position
+def updatePanelOffset(model):
+   """
+   Here we shift the nodes of the individual panel elements 
+   into the skin stringer intersection point 
+   So by t_skin/downwards 
+   """
+   return None
 
 
-for param in parameters:
-    param.valuedouble = 4
+#Offsets stringer elements to the correct position
+def updateStringerOffset(model):
+    """
+   Here we shift the nodes of the individual stringer elements 
+   into the skin stringer intersection point
+   So by the position of the elastic center upwards 
+   """
+    return None 
+
+
+
+def changeParameters(model):
+    parameters = hm.Collection(model,ent.Parameter)
+    for param in parameters:
+        param.valuedouble = 4
+    updatePanelOffset()
+    updateStringerOffset()
 
