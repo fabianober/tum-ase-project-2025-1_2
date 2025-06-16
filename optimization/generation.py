d73b52045845147acaf49f5981813de5024d89bd
YannisEndr <yannis@endress-hg.de>
2025-06-16 21:38:27 +0200

    Added data randomizer

--- optimization/generation.py
+++ optimization/generation.py
@@ -1,4 +1,5 @@
 import random
+import pandas as pd
 
 # Parameters for the optimization 
 rfPosScaling = 100
@@ -15,14 +16,31 @@ def rf_score(row):
         score = rfPosScaling * abs(row['Reserve Factor'] -RF_goal)
     return score
 
+# Extracts the panel thickness from the stringer dataframe 
 def extractThickness(df):
     thicknesses = []
     for i in range(1,6):
         thicknesses.append(df.thickness[i])
     return thicknesses
 
+# Extracts dim1 until dim4 from stringer dataframe 
 def extractDimensions(df):
     dimensions = []
     for i in range(0,5):
         dimensions.append([df.dim1[i], df.dim2[i], df.dim3[i], df.dim4[i]])
     return dimensions
+
+
+def randomizeParameters(panelThickness, stringerDims):
+    newPanelThickness = []
+    for t in panelThickness:
+        change = random.randint(-30, 30)/100
+        newPanelThickness.append(t+change)
+    newStringerDims = []
+    for stringer in stringerDims:
+        newStDim = []
+        for dim in stringer:
+            change = random.randint(-30, 30)/100
+            newStDim.append(dim+change)
+        newStringerDims.append(newStDim)
+    return newPanelThickness, newStringerDims 
