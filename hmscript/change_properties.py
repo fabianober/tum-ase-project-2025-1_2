a6ab1375125e791f2657fd9b5b868f091e6ee8d6
YannisEndr <yannis@endress-hg.de>
2025-06-16 14:15:41 +0200

    Added further progress to change properties

--- hmscript/change_properties.py
+++ hmscript/change_properties.py
@@ -5,12 +5,19 @@ model = hm.Model()
 
 
 #Offsets panel elements to the correct position
-def updatePanelOffset(model):
+def updatePanelOffset(model, t1, t2, t3, t4, t5):
    """
    Here we shift the nodes of the individual panel elements 
    into the skin stringer intersection point 
    So by t_skin/downwards 
    """
+   filter = hm.FilterByCollection(ent.Element, ent.Component)
+   panels = hm.Collection(model, ent.Component, id="id=1,2,3,4,5,6,7,8,9,10")
+   panelElements = hm.Collection(model,filter, panels)
+
+   for element in panelElements:
+        if element.component.name == 'panel1'
+            element.zoff = t1/2
    return None
 
 
@@ -21,14 +28,15 @@ def updateStringerOffset(model):
    into the skin stringer intersection point
    So by the position of the elastic center upwards 
    """
+
     return None 
 
 
 
-def changeParameters(model):
+def changeParameters(model, skinThickness, columnDimensions):
     parameters = hm.Collection(model,ent.Parameter)
     for param in parameters:
         param.valuedouble = 4
-    updatePanelOffset()
-    updateStringerOffset()
+    updatePanelOffset(model)
+    updateStringerOffset(model)
 
