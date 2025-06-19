0b1a4791dfa9720471c91d99b1ee8d9513704979
Cadet267 <di267@outlook.de>
2025-06-19 14:00:29 +0200

    Final Fixes to columnBuckReverse

--- optimization/columnBuckReverse.py
+++ optimization/columnBuckReverse.py
@@ -1,10 +1,14 @@
 import math
 import sympy
-RF_goal = 0.9
 
 #Reverse engineering of DIM1 for Euler buckling for DIM2, DIM3, DIM4 and thickness_skin unchanged
+#Test case for stringer loadcase with RF=0.4 (one iteration)
+#DIM1: 25 -> 29.9597
+#DIM2: 2 -> 19.988
+#DIM3: 20 -> 42.5966
+#thickness_skin(two solutions depending on solver setting): 4 -> 22.9725 (current setting)/1.6 
 
-def reverseColumn_Euler_DIM1_block(RF_goal, DIM02, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
+def reverseColumn_Euler_DIM1(RF_goal, DIM01, DIM02, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
     sigma_crit = RF_goal*sigma_applied
     I_by_A = abs(sigma_crit)*(c**2*length**2)/(math.pi**2*EModulus)
     DIM1 = sympy.symbols('DIM1')
@@ -34,11 +38,11 @@ def reverseColumn_Euler_DIM1_block(RF_goal, DIM02, DIM03, DIM04, stringer_pitch_
     ) / A_tot
     I_over_A = (I_y_skin + A_skin * (z_skin - z_bar)**2 + I_y_top + A_top * (z_top - z_bar)**2 + I_y_webs + 2 * A_side_web * (z_web - z_bar)**2 + I_y_bottoms + 2 * A_bottom * (z_bottom - z_bar)**2)/A_tot
     optimization = sympy.Eq(I_over_A, I_by_A)
-    DIM1_rev = sympy.nsolve(optimization, DIM1, 25)
+    DIM1_rev = sympy.nsolve(optimization, DIM1, DIM01)
     return DIM1_rev
 
 #Reverse engineering of DIM2 for Euler for DIM1, DIM3, DIM4 and thickness_skin unchanged
-def reverseColumn_Euler_DIM2(RF_goal, DIM01, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
+def reverseColumn_Euler_DIM2(RF_goal, DIM01, DIM02, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
     sigma_crit = RF_goal*sigma_applied
     I_by_A = abs(sigma_crit)*(c**2*length**2)/(math.pi**2*EModulus)
     DIM1 = DIM01
@@ -68,11 +72,11 @@ def reverseColumn_Euler_DIM2(RF_goal, DIM01, DIM03, DIM04, stringer_pitch_in, th
     ) / A_tot
     I_over_A = (I_y_skin + A_skin * (z_skin - z_bar)**2 + I_y_top + A_top * (z_top - z_bar)**2 + I_y_webs + 2 * A_side_web * (z_web - z_bar)**2 + I_y_bottoms + 2 * A_bottom * (z_bottom - z_bar)**2)/A_tot
     optimization = sympy.Eq(I_over_A, I_by_A)
-    DIM2_rev = sympy.nsolve(optimization, DIM2, 2)
+    DIM2_rev = sympy.nsolve(optimization, DIM2, DIM02)
     return DIM2_rev
 
 #Reverse engineering of DIM3 for Euler for DIM1, DIM2, DIM4 and thickness_skin unchanged
-def reverseColumn_Euler_DIM3(RF_goal, DIM01, DIM02, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
+def reverseColumn_Euler_DIM3(RF_goal, DIM01, DIM02, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
     sigma_crit = RF_goal*sigma_applied
     I_by_A = abs(sigma_crit)*(c**2*length**2)/(math.pi**2*EModulus)
     DIM1 = DIM01
@@ -102,11 +106,11 @@ def reverseColumn_Euler_DIM3(RF_goal, DIM01, DIM02, DIM04, stringer_pitch_in, th
     ) / A_tot
     I_over_A = (I_y_skin + A_skin * (z_skin - z_bar)**2 + I_y_top + A_top * (z_top - z_bar)**2 + I_y_webs + 2 * A_side_web * (z_web - z_bar)**2 + I_y_bottoms + 2 * A_bottom * (z_bottom - z_bar)**2)/A_tot
     optimization = sympy.Eq(I_over_A, I_by_A)
-    DIM3_rev = sympy.nsolve(optimization, DIM3, 20)
+    DIM3_rev = sympy.nsolve(optimization, DIM3, DIM03)
     return DIM3_rev
 
 #Reverse engineering of DIM4 for Euler for DIM1, DIM2, DIM3 and thickness_skin unchanged
-def reverseColumn_Euler_DIM4(RF_goal, DIM01, DIM02, DIM03, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
+def reverseColumn_Euler_DIM4(RF_goal, DIM01, DIM02, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
     sigma_crit = RF_goal*sigma_applied
     I_by_A = abs(sigma_crit)*(c**2*length**2)/(math.pi**2*EModulus)
     DIM1 = DIM01
@@ -136,11 +140,11 @@ def reverseColumn_Euler_DIM4(RF_goal, DIM01, DIM02, DIM03, stringer_pitch_in, th
     ) / A_tot
     I_over_A = (I_y_skin + A_skin * (z_skin - z_bar)**2 + I_y_top + A_top * (z_top - z_bar)**2 + I_y_webs + 2 * A_side_web * (z_web - z_bar)**2 + I_y_bottoms + 2 * A_bottom * (z_bottom - z_bar)**2)/A_tot
     optimization = sympy.Eq(I_over_A, I_by_A)
-    DIM4_rev = sympy.nsolve(optimization, DIM4, 15)
+    DIM4_rev = sympy.nsolve(optimization, DIM4, DIM04)
     return DIM4_rev
 
 #Reverse engineering of thickness_skin for Euler for DIM1, DIM2, DIM2 and DIM4 unchanged
-def reverseColumn_Euler_thickness_skin(RF_goal, DIM01, DIM02, DIM03, DIM04, stringer_pitch_in, length, c, EModulus, sigma_applied):
+def reverseColumn_Euler_thickness_skin(RF_goal, DIM01, DIM02, DIM03, DIM04, stringer_pitch_in, thickness_skin_in, length, c, EModulus, sigma_applied):
     sigma_crit = RF_goal*sigma_applied
     I_by_A = abs(sigma_crit)*(c**2*length**2)/(math.pi**2*EModulus)
     DIM1 = DIM01
@@ -170,11 +174,17 @@ def reverseColumn_Euler_thickness_skin(RF_goal, DIM01, DIM02, DIM03, DIM04, stri
     ) / A_tot
     I_over_A = (I_y_skin + A_skin * (z_skin - z_bar)**2 + I_y_top + A_top * (z_top - z_bar)**2 + I_y_webs + 2 * A_side_web * (z_web - z_bar)**2 + I_y_bottoms + 2 * A_bottom * (z_bottom - z_bar)**2)/A_tot
     optimization = sympy.Eq(I_over_A, I_by_A)
-    thickness_skin_rev = sympy.nsolve(optimization, thickness_skin, 4)
+    thickness_skin_rev = sympy.nsolve(optimization, thickness_skin, (0, 7*thickness_skin_in), solver='bisect')
     return thickness_skin_rev
 #Has to be decided if thickness_skin should be rverse engineered for column buckling, biaxial panel buckling and shear panel buckling and then the maximum is used for optimization
 
 #testcase for column buckling reverse engineering
 if __name__ == '__main__':
-    testopt_DIM1 = reverseColumn_Euler_DIM1_block(RF_goal=0.9, DIM02=2, DIM03=20, DIM04=15, stringer_pitch_in=200, thickness_skin_in=4, length=750, c=1, EModulus=65669.47, sigma_applied=-83.09)
+    testopt_DIM1 = reverseColumn_Euler_DIM1(RF_goal=0.9, DIM01=25, DIM02=2, DIM03=20, DIM04=15, stringer_pitch_in=200, thickness_skin_in=4, length=750, c=1, EModulus=65669.47, sigma_applied=-83.09)
     print(testopt_DIM1)
+    testopt_DIM2 = reverseColumn_Euler_DIM2(RF_goal=0.9, DIM01=25, DIM02=2, DIM03=20, DIM04=15, stringer_pitch_in=200, thickness_skin_in=4, length=750, c=1, EModulus=65669.47, sigma_applied=-83.09)
+    print(testopt_DIM2)
+    testopt_DIM3 = reverseColumn_Euler_DIM3(RF_goal=0.9, DIM01=25, DIM02=2, DIM03=20, DIM04=15, stringer_pitch_in=200, thickness_skin_in=4, length=750, c=1, EModulus=65669.47, sigma_applied=-83.09)
+    print(testopt_DIM3)
+    testopt_thickness = reverseColumn_Euler_thickness_skin(RF_goal=0.9, DIM01=25, DIM02=2, DIM03=20, DIM04=15, stringer_pitch_in=200, thickness_skin_in=4, length=750, c=1, EModulus=65669.47, sigma_applied=-83.09)
+    print(testopt_thickness)
